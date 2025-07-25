fafdc700418c6825b1baaf767c710eb5837aef4a
OnkarShinde <shindeo522@gmail.com>
2025-07-26 02:20:23 +0530

    confluence file

--- creating_RDS/creating_RDS.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_db_instance" "example" {
-    allocated_storage = 20
-    engine = "mysql"
-    instance_class = "db.t2.micro"
-    username = "admin"
-    password = "admin"
-    skip_final_snapshot = true
-}
--- provision_EKS/provision_EKS.tf
+++
@@ -1,6 +0,0 @@
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  cluster_name = "EKS_Test_cluster"
-  cluster_version = "1.24"
-  subnet_ids = ["subnet-12345678", "subnet-87654321"]
-}
